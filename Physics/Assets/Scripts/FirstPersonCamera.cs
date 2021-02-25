fc77d385c6497b15e39de9d7494b4870c83d1dc4
Logan Ryan <loganryan982@gmail.com>
2021-02-25 14:00:52 +1100

    Created first person camera

---
+++ Physics/Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    float rotationOnX;
+    float mouseSensitivity = 90f;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+        float m_X = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+
+        rotationOnX -= mouseY;
+        rotationOnX = Mathf.Clamp(rotationOnX, -90f, 90f);
+        transform.localEulerAngles = new Vector3(rotationOnX, 0f, 0f);
+
+        player.Rotate(Vector3.up * m_X);
+    }
+}
---
+++ Physics/Assets/Scripts/Gernade.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Gernade : MonoBehaviour
+{
+    Rigidbody rb = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb.AddExplosionForce(700f, transform.position, 5f);
+        }
+    }
+}
